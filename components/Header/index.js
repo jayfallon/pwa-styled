ec865d619284eb3f4bbf042e97973df447dc0d1b
Jay Fallon <jayfallon@gmail.com>
2019-05-24 20:39:36 -0400

    refactored header and package msgs

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,32 @@
-const Header = props => (
-    <p>header</p>
-)
+import React, { Component } from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+  };
+  Router.onRouteChangeComplete = () => {
+    NProgress.done();
+  };
+  Router.onRouteChangeError = () => {
+    NProgress.done();
+  };
+
+  class Header extends Component {
+    render() {
+      return (
+        <>
+          
+          <header role="banner">
+            <nav className="header__nav" role="navigation">
+              <p>nav here</p>
+            </nav>
+          </header>
+        </>
+      );
+    }
+  }
 
 export default Header
